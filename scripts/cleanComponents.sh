40cd4f1c5af54c75acd3068843a5c2d1a797a2fc
Alejandro Lopez <alexjes6543@gmail.com>
2024-03-20 15:38:55 -0400

    SFC-31 Modify creation scripts to point new repositories. (#1)
    * Modify creation scripts to point new repositories.
    
    * add missing folders references

--- scripts/cleanComponents.sh
+++ scripts/cleanComponents.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Delete the gpt-rag-ingestion folder from .azure if it exists
-if [ -d ./.azure/gpt-rag-ingestion ]; then
-    rm -rf ./.azure/gpt-rag-ingestion
+# Delete the gpt-rag-ingestion folder from .salesfactory if it exists
+if [ -d ./.salesfactory/gpt-rag-ingestion ]; then
+    rm -rf ./.salesfactory/gpt-rag-ingestion
 fi
 
-# Delete the gpt-rag-orchestrator folder from .azure if it exists
-if [ -d ./.azure/gpt-rag-orchestrator ]; then
-    rm -rf ./.azure/gpt-rag-orchestrator
+# Delete the gpt-rag-orchestrator folder from .salesfactory if it exists
+if [ -d ./.salesfactory/gpt-rag-orchestrator ]; then
+    rm -rf ./.salesfactory/gpt-rag-orchestrator
 fi
 
-# Delete the gpt-rag-frontend folder from .azure if it exists
-if [ -d ./.azure/gpt-rag-frontend ]; then
-    rm -rf ./.azure/gpt-rag-frontend
+# Delete the gpt-rag-frontend folder from .salesfactory if it exists
+if [ -d ./.salesfactory/gpt-rag-frontend ]; then
+    rm -rf ./.salesfactory/gpt-rag-frontend
 fi
--- scripts/fetchComponents.ps1
+++ scripts/fetchComponents.ps1
@@ -1,23 +1,23 @@
-# Delete the gpt-rag-ingestion folder from .azure if it exists
-if (Test-Path -Path ".\.azure\gpt-rag-ingestion") {
-    Remove-Item -Path ".\.azure\gpt-rag-ingestion" -Recurse -Force
+# Delete the gpt-rag-ingestion folder from .salesfactory if it exists
+if (Test-Path -Path ".\.salesfactory\gpt-rag-ingestion") {
+    Remove-Item -Path ".\.salesfactory\gpt-rag-ingestion" -Recurse -Force
 }
 
 # Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-ingestion .\.azure\gpt-rag-ingestion
+git clone https://github.com/Salesfactory/gpt-rag-ingestion .\.salesfactory\gpt-rag-ingestion
 
-# Delete the gpt-rag-orchestrator folder from .azure if it exists
-if (Test-Path -Path ".\.azure\gpt-rag-orchestrator") {
-    Remove-Item -Path ".\.azure\gpt-rag-orchestrator" -Recurse -Force
+# Delete the gpt-rag-orchestrator folder from .salesfactory if it exists
+if (Test-Path -Path ".\.salesfactory\gpt-rag-orchestrator") {
+    Remove-Item -Path ".\.salesfactory\gpt-rag-orchestrator" -Recurse -Force
 }
 
 # Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-orchestrator .\.azure\gpt-rag-orchestrator
+git clone https://github.com/Salesfactory/gpt-rag-orchestrator .\.salesfactory\gpt-rag-orchestrator
 
-# Delete the gpt-rag-frontend folder from .azure if it exists
-if (Test-Path -Path ".\.azure\gpt-rag-frontend") {
-    Remove-Item -Path ".\.azure\gpt-rag-frontend" -Recurse -Force
+# Delete the gpt-rag-frontend folder from .salesfactory if it exists
+if (Test-Path -Path ".\.salesfactory\gpt-rag-frontend") {
+    Remove-Item -Path ".\.salesfactory\gpt-rag-frontend" -Recurse -Force
 }
 
 # Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-frontend .\.azure\gpt-rag-frontend
+git clone https://github.com/Salesfactory/gpt-rag-frontend .\.salesfactory\gpt-rag-frontend
